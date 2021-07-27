0f666a95392bf7d3f53c7c842bb8d4cf66992add
upinetree <upinetree@gmail.com>
2021-07-27 23:27:58 +0900

    fix conditions

--- actions/lint_pr_body.sh
+++ actions/lint_pr_body.sh
@@ -4,14 +4,14 @@ set -xu
 
 git diff origin/$GITHUB_BASE_REF HEAD --name-only --diff-filter=AM | grep -v $TARGET_FILES_PATTERN
 
-if $?; then
+if [ $? -eq 0 ]; then
   echo "no target files!"
   exit 0
 fi
 
 echo "$PULL_REQUEST_BODY" | tr -d '\r\n' | grep $VALID_PATTERN
 
-if $?; then
+if [ $? -eq 0 ]; then
   echo "valid pull request body!"
   exit 0
 fi
